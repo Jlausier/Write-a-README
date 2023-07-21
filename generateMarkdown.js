89828cbbac2e7e6e9be9f3d0a6416755195937f8
Jacob <jakelausier7878@gmail.com>
2023-07-21 19:52:30 -0400

    formated with prettier

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,19 +1,17 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-
-  switch(license){
-    case 'MIT':
-      return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
-    case 'Apache 2.0':
-      return '[![License: Apache 2.0](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
-    case 'GNU GPLv3':
-      return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
-    case 'ISC':
-      return '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)';
+  switch (license) {
+    case "MIT":
+      return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+    case "Apache 2.0":
+      return "[![License: Apache 2.0](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+    case "GNU GPLv3":
+      return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+    case "ISC":
+      return "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)";
     default:
-      return ''; // Empty string for 'None' license
-  
+      return ""; // Empty string for 'None' license
   }
 }
 
@@ -21,30 +19,31 @@ function renderLicenseBadge(license) {
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
   switch (license) {
-    case 'MIT':
-      return 'https://opensource.org/licenses/MIT';
-    case 'Apache 2.0':
-      return 'https://opensource.org/licenses/Apache-2.0';
-    case 'GNU GPLv3':
-      return 'https://www.gnu.org/licenses/gpl-3.0';
-    case 'ISC':
-      return 'https://opensource.org/licenses/ISC';
+    case "MIT":
+      return "https://opensource.org/licenses/MIT";
+    case "Apache 2.0":
+      return "https://opensource.org/licenses/Apache-2.0";
+    case "GNU GPLv3":
+      return "https://www.gnu.org/licenses/gpl-3.0";
+    case "ISC":
+      return "https://opensource.org/licenses/ISC";
     default:
-      return ''; // Empty string for 'None' license
+      return ""; // Empty string for 'None' license
   }
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  if (license === 'None') {
-    return '';
+  if (license === "None") {
+    return "";
   }
   return `## License
 
-This project is licensed under the [${license}](${renderLicenseLink(license)}) License.`;
+This project is licensed under the [${license}](${renderLicenseLink(
+    license
+  )}) License.`;
 }
-
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -80,8 +79,6 @@ function generateMarkdown(data) {
   - GitHub: [${data.username}](https://github.com/${data.username})
   - Email: ${data.email}
   `;
-  };
-
-
+}
 
 module.exports = generateMarkdown;
--- index.js
+++ index.js
@@ -1,111 +1,106 @@
 // TODO: Include packages needed for this application
-const fs = require('fs');
-const inquirer = require('inquirer');
+const fs = require("fs");
+const inquirer = require("inquirer");
 
-const generateMarkdown = require('./generateMarkdown');
-
+const generateMarkdown = require("./generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = [
-{
-    type: 'input', 
-    name: 'title',
-    message: 'Enter the title of your project:',
+  {
+    type: "input",
+    name: "title",
+    message: "Enter the title of your project:",
     validate: function (input) {
-        if (input.trim() === '') {
-            return 'This field is required.';
-        }
-        return true;
-    }
-},
-{
-    type: 'input',
-    name: 'description',
-    message: 'Enter a description of your project:',
+      if (input.trim() === "") {
+        return "This field is required.";
+      }
+      return true;
+    },
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Enter a description of your project:",
     validate: function (input) {
-        if (input.trim() === '') {
-            return 'This field is required.';
-        }
-        return true;
-    }
-
-},
-{
-    type: 'input',
-    name: 'installation',
-    message: 'Enter installation instructions:',
-    default: 'No specific installation at the moment.'
-},
-{
-    type: 'input',
-    name: 'usage',
-    message: 'Enter usage information:',
-    default: 'No specific usage at the moment.'
-},
-{
-    type: 'input',
-    name: 'contributing',
-    message: 'Enter contribution guidelines:',
-    default: 'No specific contribution guidlines at the moment.'
-},
-{
-type: 'input',
-name: 'tests',
-message:'Enter test details',
-default: 'No specific tests at the moment.',
-
-},
-{
-    type: 'list',
-    name: 'license',
-    message: 'Choose a license for your project:',
-    choices: ['MIT', 'Apache 2.0', 'GNU GPLv3', 'ISC', 'None']
-},
-{
-    type: 'input',
-    name: 'username',
-    message: 'Enter your GitHub username:',
+      if (input.trim() === "") {
+        return "This field is required.";
+      }
+      return true;
+    },
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "Enter installation instructions:",
+    default: "No specific installation at the moment.",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Enter usage information:",
+    default: "No specific usage at the moment.",
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "Enter contribution guidelines:",
+    default: "No specific contribution guidlines at the moment.",
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "Enter test details",
+    default: "No specific tests at the moment.",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Choose a license for your project:",
+    choices: ["MIT", "Apache 2.0", "GNU GPLv3", "ISC", "None"],
+  },
+  {
+    type: "input",
+    name: "username",
+    message: "Enter your GitHub username:",
     validate: function (input) {
-        if (input.trim() === '') {
-            return 'This field is required.';
-        }
-        return true;
-    }
-},
-{
-    type: 'input',
-    name: 'email',
-    message: 'Enter your email address:',
+      if (input.trim() === "") {
+        return "This field is required.";
+      }
+      return true;
+    },
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Enter your email address:",
     validate: function (input) {
-        if (input.trim() === '') {
-            return 'This field is required.';
-        }
-        return true;
-    }
-
-}];
+      if (input.trim() === "") {
+        return "This field is required.";
+      }
+      return true;
+    },
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, err => {
-        if(err){
-            console.error(err)
-        } else {
-            console.log('README.me generated successfully!');
-        }
-    })
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log("README.me generated successfully!");
+    }
+  });
 }
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer.prompt(questions).then(answers => {
-       
-        const markdoownContent = generateMarkdown(answers)
-   
-        writeToFile('README.md', markdoownContent);
-      });
-}
+  inquirer.prompt(questions).then((answers) => {
+    const markdoownContent = generateMarkdown(answers);
 
+    writeToFile("README.md", markdoownContent);
+  });
+}
 
 // Function call to initialize app
 init();
